14818d000679d2c158ce1647023e45ca4793d5a0
Mamdouh <m.mamdouh@symbyo.com>
2017-10-22 14:30:48 +0200

    add sortable list

--- App.js
+++ App.js
@@ -1,57 +1,81 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+import SortableListView from 'react-native-sortable-listview'
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+let data = {
+  1: { text: 1 },
+  2: { text: 2 },
+  3: { text: 3 },
+  4: { text: 4 },
+  5: { text: 5 },
+  6: { text: 6 },
+  7: { text: 7 },
+  8: { text: 8 },
+  9: { text: 9 },
+  10: { text: 10 },
+  a: { text: 'a' },
+  b: { text: 'b' },
+  c: { text: 'c' },
+  d: { text: 'd' },
+  e: { text: 'e' },
+  f: { text: 'f' },
+  g: { text: 'g' },
+  h: { text: 'h' },
+  i: { text: 'i' },
+  j: { text: 'j' },
+  k: { text: 'k' },
+}
 
-export default class App extends Component<{}> {
+let order = Object.keys(data) //Array of keys
+
+class RowComponent extends Component {
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+      <TouchableHighlight
+        underlayColor={'#eee'}
+        style={{
+          padding: 25,
+          backgroundColor: 'red',
+          borderBottomWidth: 1,
+          borderColor: '#eee',
+          margin: 30,
+        }}
+        {...this.props.sortHandlers}
+      >
+        <Text>{this.props.data.text}</Text>
+      </TouchableHighlight>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <SortableListView
+        style={{ flex: 1 }}
+        data={data}
+        order={order}
+        onRowMoved={e => {
+          order.splice(e.to, 0, order.splice(e.from, 1)[0])
+          this.forceUpdate()
+        }}
+        renderRow={row => <RowComponent data={row} />}
+        //ref="sortableList"
+        ref={(sortableListView) => { this.sortableListView = sortableListView; }}
+      />
+    )
+  }
+}
+
+export default App
